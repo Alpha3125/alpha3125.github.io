e5eae30e104eafd6754015f0bea2fa81714c6ede
Girija Sankar Ray <girija4m@gmail.com>
2026-07-22 00:48:53 +0530

    nnn

--- team.js
+++ team.js
@@ -1,5 +1,6 @@
-const API =
-    "https://script.google.com/macros/s/AKfycbxmWEsq3epM3h9MI_v3G9Cy451z-0wts3zcwoZf_gHpSyqmR6L4A3LujdzmLGpzOqq5/exec";
+console.log("team.js loaded");
+
+const API = "https://script.google.com/macros/s/AKfycbxmWEsq3epM3h9MI_v3G9Cy451z-0wts3zcwoZf_gHpSyqmR6L4A3LujdzmLGpzOqq5/exec";
 
 async function loadMembers() {
     const response = await fetch(API);
