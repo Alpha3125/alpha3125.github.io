5e14a346e6e0c876b34779957bfefe9540cb9adf
Girija Sankar Ray <girija4m@gmail.com>
2026-07-22 00:43:17 +0530

    ..

--- team.js
+++ team.js
@@ -1,5 +1,5 @@
 const API =
-    "https://script.google.com/u/0/home/projects/1UmcjzjF2VitfaK80rbe7SEDCCrEf7fhXiM3m01mG9zOUaMnbrBUAF9KW";
+    "https://script.google.com/u/0/home/projects/1UmcjzjF2VitfaK80rbe7SEDCCrEf7fhXiM3m01mG9zOUaMnbrBUAF9KW/";
 
 async function loadMembers() {
     const response = await fetch(API);
